99e3e8a14bfae3e46c11c30d765a06bd11f2169f
AlveGames <renjiYT1978@gmail.com>
2026-05-01 15:15:59 -0500

    Update __init__.py

--- Practica2/core/__init__.py
+++ Practica2/core/__init__.py
@@ -1,7 +1,8 @@
 # Paquete de infraestructura transversal: persistencia, interfaces, mixins y decoradores.
-from .decorators import ask_continue
+from .decorators import ask_continue, confirm_save
 from .interfaces import CrudInterface
 from .json_manager import JsonManager
 from .mixins import ValidationMixin, LogMixin
-
-__all__ = ["ask_continue", "CrudInterface", "JsonManager", "ValidationMixin", "LogMixin"]
+ 
+__all__ = ["ask_continue", "confirm_save", "CrudInterface", "JsonManager", "ValidationMixin", "LogMixin"]
+ 
