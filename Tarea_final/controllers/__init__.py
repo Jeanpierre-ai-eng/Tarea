763eae01667a5c14b8694481fff3fdd3707ff651
jcepedaa2 <jcepedaa2@unemi.edu.ec>
2026-05-07 17:54:37 -0500

    __init__.py

--- Tarea_final/controllers/__init__.py
+++ Tarea_final/controllers/__init__.py
@@ -1,4 +1,3 @@
-# Paquete de controladores: encapsulan la lógica de negocio del sistema.
 from .employee_controller import EmployeeController
 from .leave_type_controller import LeaveTypeController
 from .leave_controller import LeaveController
